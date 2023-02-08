c2759a809e789bf80fcca2b7165015cfd71cc983
Dyan Carvalho <soudyancarvalho@gmail.com>
2023-02-08 17:43:41 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -20,8 +20,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
 	public User() {
 	}
+	
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
